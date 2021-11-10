6ed7652b66cd5e87c7735a4a2be9168eabeec655
Piyush Pandey <piyushpvns@gmail.com>
2021-11-10 13:34:52 +0530

    Dispatching Actions

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+//packages
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 
+//local files
 import './index.css';
 import App from './components/App';
 import movies from './reducers' 
@@ -9,11 +11,20 @@ import movies from './reducers'
 //create store(by redux)
 const store = createStore(movies);
 console.log('store', store);
-console.log('STATE', store.getState());
+// console.log('BEFORE STATE', store.getState());
+
+// //dispatching action
+// store.dispatch({
+//   type: 'ADD_MOVIES',
+//   movies : [{name: 'Superman'}]
+// });
+
+// console.log('AFTER STATE', store.getState());
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
