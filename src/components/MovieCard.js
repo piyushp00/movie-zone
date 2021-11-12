1ccc8496f3f58c2dd80c9077a5d74d87455864fd
Piyush Pandey <piyushpvns@gmail.com>
2021-11-13 00:40:35 +0530

    handle clicks

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { addToFavorites, removeFromFavorites } from "../actions";
 
 class MovieCard extends React.Component {
-  render() {
+  handleFavoriteClick = () => {
     const { movie } = this.props;
+    this.props.dispatch(addToFavorites(movie));
+  }; 
+
+  handleUnFavoriteClick = () => {
+    const { movie } = this.props;
+    this.props.dispatch(removeFromFavorites(movie));
+  };
+
+  render() {
+    const { movie, isFavorite } = this.props;
     return (
       <div className="movie-card">
         <div className="left">
@@ -15,7 +26,21 @@ class MovieCard extends React.Component {
           <div className="plot">{movie.Plot}</div>
           <div className="footer">
             <div className="rating">{movie.imdbRating}</div>
-            <button className="favourite-btn">Favorite</button>
+            {isFavorite ? (
+              <button
+                className="unfavourite-btn"
+                onClick={this.handleUnFavoriteClick}
+              >
+                UnFavorite
+              </button>
+            ) : (
+              <button
+                className="favourite-btn"
+                onClick={this.handleFavoriteClick}
+              >
+                Favorite
+              </button>
+            )}
           </div>
         </div>
         <div></div>
