18e1d252f4004d96139e162ce33db419d6bcb0f8
Piyush Pandey <piyushpvns@gmail.com>
2021-11-13 00:41:07 +0530

    add reducers for add and removing fav

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,47 @@
-import { ADD_MOVIES } from '../actions';
+import {
+  ADD_MOVIES,
+  ADD_TO_FAVORITES,
+  REMOVE_FROM_FAVORITES,
+} from "../actions";
 
 const initialMovieState = {
-    list: [], 
-    favourites: []
+  list: [],
+  favourites: [],
+};
+
+export default function movies(state = initialMovieState, action) {
+  // if(action.type === ADD_MOVIES){
+  //     return {
+  //         ...state,
+  //         list: action.movies
+  //     }
+  // }
+  // return state;
+
+  switch (action.type) {
+    case ADD_MOVIES:
+      return {
+        ...state,
+        list: action.movies,
+      };
+
+    case ADD_TO_FAVORITES:
+      return {
+        ...state,
+        favourites: [action.movie, ...state.favourites],
+      };
+
+    case REMOVE_FROM_FAVORITES:
+      const filteredArray = state.favourites.filter(
+        (movie) => movie.Title !== action.movie.Title
+      );
+
+      return {
+        ...state,
+        favourites: filteredArray,
+      };
+
+    default:
+      return state;
+  }
 }
-
-export default function movies (state = initialMovieState, action){
-    if(action.type === ADD_MOVIES){
-        return {
-            ...state,
-            list: action.movies
-        }
-    }
-    return state;
-}  
-
- 
