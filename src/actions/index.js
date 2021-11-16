bf520c6204918fbe2de3198bca0b499adb98d7eb
Piyush Pandey <piyushpvns@gmail.com>
2021-11-16 23:58:09 +0530

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,12 @@
-{
-    type: 'ADD_MOVIES'
-    movies:[m1, m2, m3]
-}
+// {
+//     type: 'ADD_MOVIES'
+//     movies:[m1, m2, m3]
+// }
 
-{
-    type: 'DECREASE_COUNT'
-    user:{}
-}
+// {
+//     type: 'DECREASE_COUNT'
+//     user:{}
+// }
 
 //action types
 export const ADD_MOVIES = "ADD_MOVIES";
